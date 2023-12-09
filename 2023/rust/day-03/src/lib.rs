39e598f7fc70c8432be6a706ab761cc66f00ebe1
Guy Waldman <6546430+guywaldman@users.noreply.github.com>
2023-12-09 17:05:17 +0200

    Add part 2 solution

--- 2023/rust/day-03/src/lib.rs
+++ 2023/rust/day-03/src/lib.rs
@@ -1,3 +1,5 @@
+#![feature(slice_group_by)]
+
 use itertools::iproduct;
 use std::error::Error;
 
@@ -13,6 +15,14 @@ struct Cell {
     content: CellContent,
     x: usize,
     y: usize,
+}
+
+#[derive(Debug)]
+struct GridNumber {
+    cells: Vec<Cell>,
+    x_range: (usize, usize), // (start, end)
+    y: usize,
+    value: usize,
 }
 
 #[derive(Debug)]
@@ -61,66 +71,61 @@ impl Grid {
                 neighbors.push(cell);
             }
         }
+        neighbors.sort_by(|a, b| a.y.cmp(&b.y).then(a.x.cmp(&b.x)));
         neighbors
+    }
+
+    fn get_numbers(&self) -> Vec<GridNumber> {
+        let mut numbers = Vec::new();
+
+        for (y, row) in self.cells.iter().enumerate() {
+            let mut x = 0;
+            while x < row.len() {
+                let cell = self.get_cell(x, y).unwrap();
+                if let CellContent::Digit(_) = cell.content {
+                    let x_start = x;
+                    let mut xd = 0;
+                    let mut cells = Vec::with_capacity(self.cells[0].len());
+                    let mut digits = Vec::with_capacity(self.cells[0].len());
+                    while let Some(Cell {
+                        content: CellContent::Digit(c),
+                        x,
+                        y,
+                    }) = self.get_cell(x + xd, y)
+                    {
+                        cells.push(Cell {
+                            content: CellContent::Digit(*c),
+                            x: *x,
+                            y: *y,
+                        });
+                        digits.push(*c);
+                        xd += 1;
+                    }
+                    let value = digits.iter().fold(0, |acc, digit| acc * 10 + *digit as usize);
+                    numbers.push(GridNumber {
+                        cells,
+                        value,
+                        x_range: (x_start, x + xd),
+                        y,
+                    });
+                    x += xd;
+                }
+                x += 1;
+            }
+        }
+
+        numbers
     }
 }
 
 pub fn solve_part_1(input: &str) -> Result<String, Box<dyn Error>> {
     let grid = Grid::new(input);
-    let mut potential_part_numbers: Vec<Vec<Cell>> = Vec::new();
+    let all_grid_numbers = grid.get_numbers();
 
-    for (y, row) in grid.cells.iter().enumerate() {
-        let mut x = 0;
-        while x < row.len() {
-            let cell = grid.get_cell(x, y).unwrap();
-            if let CellContent::Digit(_) = cell.content {
-                let mut xd = 0;
-                let mut digits = Vec::with_capacity(grid.cells[0].len());
-                while let Some(Cell {
-                    content: CellContent::Digit(c),
-                    x,
-                    y,
-                }) = grid.get_cell(x + xd, y)
-                {
-                    digits.push(Cell {
-                        content: CellContent::Digit(*c),
-                        x: *x,
-                        y: *y,
-                    });
-                    xd += 1;
-                }
-                potential_part_numbers.push(digits);
-                x += xd;
-            }
-            x += 1;
-
-            // if let CellContent::Digit(c) = cell.content {
-            //     let neighbors = grid.get_neighbors(x, y);
-            //     let is_adjacent_to_symbol = neighbors.iter().any(|n|
-            // matches!(n.content, CellContent::Symbol(_)));
-            //     if is_adjacent_to_symbol {
-            //         is_in_number = false;
-            //         digits.clear();
-            //         x += 1;
-            //         continue;
-            //     }
-            //     digits.push(c);
-            //     is_in_number = true;
-            // } else if is_in_number {
-            //     // Current cell is not a digit and we were in a number, so we
-            // just finished a     // number.
-            //     part_numbers.push(digits.clone());
-            //     digits.clear();
-            //     is_in_number = false;
-            // }
-            // x += 1;
-        }
-    }
-
-    let part_numbers = potential_part_numbers
+    let part_numbers = all_grid_numbers
         .iter()
-        .filter(|number| {
-            number.iter().any(|c| {
+        .filter(|num| {
+            num.cells.iter().any(|c| {
                 let neighbors = grid.get_neighbors(c.x, c.y);
                 neighbors.iter().any(|n| matches!(n.content, CellContent::Symbol(_)))
             })
@@ -129,8 +134,8 @@ pub fn solve_part_1(input: &str) -> Result<String, Box<dyn Error>> {
 
     let result: usize = part_numbers
         .iter()
-        .map(|d| {
-            d.iter().filter_map(|c| match c {
+        .map(|num| {
+            num.cells.iter().filter_map(|c| match c {
                 Cell {
                     content: CellContent::Digit(c),
                     ..
@@ -144,8 +149,37 @@ pub fn solve_part_1(input: &str) -> Result<String, Box<dyn Error>> {
     Ok(result.to_string())
 }
 
-pub fn solve_part_2(_input: &str) -> Result<String, Box<dyn Error>> {
-    todo!()
+pub fn solve_part_2(input: &str) -> Result<String, Box<dyn Error>> {
+    let grid = Grid::new(input);
+    let numbers = grid.get_numbers();
+    let mut sum = 0;
+
+    for (y, row) in grid.cells.iter().enumerate() {
+        for (x, cell) in row.iter().enumerate() {
+            if let CellContent::Symbol('*') = cell.content {
+                let mut neighboring_numbers: Vec<&GridNumber> = Vec::new();
+                for Cell { content, x, y } in grid.get_neighbors(x, y) {
+                    if let CellContent::Digit(_) = content {
+                        let number_for_digit = numbers.iter().find(|n| n.x_range.0 <= *x && n.x_range.1 >= *x && n.y == *y);
+                        if let Some(number) = number_for_digit {
+                            // Check if the number is already in the list
+                            if !neighboring_numbers.iter().any(|&n| n.x_range.0 == number.x_range.0 && n.y == number.y) {
+                                neighboring_numbers.push(number);
+                            }
+                        }
+                    }
+                }
+                if neighboring_numbers.len() != 2 {
+                    continue;
+                }
+                let gear_ratio = neighboring_numbers.iter().fold(1, |acc, n| acc * n.value);
+                sum += gear_ratio;
+            }
+        }
+    }
+
+
+    Ok(sum.to_string())
 }
 
 #[cfg(test)]
--- 2023/rust/day-03/src/main.rs
+++ 2023/rust/day-03/src/main.rs
@@ -23,9 +23,9 @@ fn main() {
 mod day03_tests {
     use super::*;
     const PART1_TEST_SOLUTION: &str = "4361";
-    const PART1_PUZZLE_SOLUTION: &str = "";
-    const PART2_TEST_SOLUTION: &str = "";
-    const PART2_PUZZLE_SOLUTION: &str = "";
+    const PART1_PUZZLE_SOLUTION: &str = "557705";
+    const PART2_TEST_SOLUTION: &str = "467835";
+    const PART2_PUZZLE_SOLUTION: &str = "84266818";
 
     #[test]
     fn part_1_test_input() {
@@ -33,21 +33,20 @@ mod day03_tests {
         assert_eq!(solve_part_1(test_input).unwrap(), PART1_TEST_SOLUTION);
     }
 
-    // #[test]
-    // fn part_1_puzzle_input() {
-    //     let puzzle_input = include_str!("../inputs/part1_puzzle.txt");
-    //     assert_eq!(solve_part_1(puzzle_input).unwrap(), PART1_PUZZLE_SOLUTION);
-    // }
+    #[test]
+    fn part_1_puzzle_input() {
+        let puzzle_input = include_str!("../inputs/part1_puzzle.txt");
+        assert_eq!(solve_part_1(puzzle_input).unwrap(), PART1_PUZZLE_SOLUTION);
+    }
 
-    // #[test]
-    // fn part_2_test_input() {
-    //     let test_input = include_str!("../inputs/part2_test.txt");
-    //     assert_eq!(solve_part_2(test_input).unwrap(), PART2_TEST_SOLUTION);
-    // }
+    #[test]
+    fn part_2_test_input() {
+        let test_input = include_str!("../inputs/part2_test.txt");
+        assert_eq!(solve_part_2(test_input).unwrap(), PART2_TEST_SOLUTION);
+    }
 
-    // #[test]
-    // fn part_2_puzzle_input() {
-    //     let puzzle_input = include_str!("../inputs/part2_puzzle.txt");
-    //     assert_eq!(solve_part_2(puzzle_input).unwrap(), PART2_PUZZLE_SOLUTION);
-    // }
+    #[test]
+    fn part_2_puzzle_input() {
+        let puzzle_input = include_str!("../inputs/part2_puzzle.txt");
+        assert_eq!(solve_part_2(puzzle_input).unwrap(), PART2_PUZZLE_SOLUTION); }
 }
